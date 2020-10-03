19c00e9d748fb60a607f9d1b1a9f77cd75c4f154
tanaychaturvedi <tanaychaturvedi08@gmail.com>
2020-10-03 20:30:58 +0530

    variable name changed

--- three sum.cpp
+++ three sum.cpp
@@ -1,7 +1,7 @@
-int Solution::threeSumClosest(vector<int> &A, int B) {
+`	int Solution::threeSumClosest(vector<int> &A, int B) {
     sort(A.begin(),A.end());
     int n=A.size();
-    int sumi=B,mini=INT_MAX;
+    int sum=B,mini=INT_MAX;
     for (int i=0;i<n-2;i++){
         int l=i+1,r=n-1;
         int s;
@@ -9,7 +9,7 @@ int Solution::threeSumClosest(vector<int> &A, int B) {
         s=A[i]+A[l]+A[r];
         if(abs(s-B)<mini){
             mini=min(mini,abs(s-B));
-            sumi=s;
+            sum=s;
         }
     
         if(s<B){
@@ -20,6 +20,6 @@ int Solution::threeSumClosest(vector<int> &A, int B) {
     }
         
     }
-    return sumi;
+    return sum;
 }
 
